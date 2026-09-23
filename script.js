6d58006d3d897cf447203e86c6deb8166e6944f0
rtbonline8-dotcom <rtbonline8@gmail.com>
2026-09-23 23:41:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwRYTofrqiijrsYg2A-hIBflqWr6Lpkg-v9ejn0RpuzxNztDGyowtHiinOZMD1flxoC/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwFXS5uhC9CNUH2wF60vTC9SuDARbbZG6DsaiGqXOzUZ1DMnqXaQ80vPxSN-ZUnc4MB/exec';
 const CSV_URL = 'YOUR_CSV_URL';
 
 document.addEventListener('DOMContentLoaded', () => {
